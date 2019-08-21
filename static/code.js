fb8a59524df830b7741ce0b6663e1cc4eac5bc6c
fdev31 <fdev31@gmail.com>
2019-08-21 23:34:59 +0200

    replace paho with mqtt.js

--- static/code.js
+++ static/code.js
@@ -23,14 +23,14 @@ function sendText(keypress) {
     if (keypress.code == 'Enter') {
         const text = document.getElementById('input_text').value;
         const payload = JSON.stringify({'author': userName, 'text': text});
-        client.publish('rooms/main/newtext', payload, 1, true);
+        client.publish('rooms/main/newtext', payload, {qos: 1, retain: true});
         document.getElementById('input_text').value = '';
     }
 }
 
-function messageArrived(msg) {
-    if (msg.topic == 'rooms/main/newtext') {
-        const payload = JSON.parse(msg.payloadString);
+function messageArrived(topic, msg) {
+    if (topic == 'rooms/main/newtext') {
+        const payload = JSON.parse(msg.toString());
         const old_size = users.size;
         users.add(payload.author);
         if (old_size < users.size) {
@@ -38,9 +38,9 @@ function messageArrived(msg) {
         }
         messagesLog.push(`<b>${payload.author}</b>: ${payload.text}`);
         showMessages();
-    } else if (msg.topic.match(/^rooms/)) {
+    } else if (topic.match(/^rooms/)) {
         const old_size = rooms.size;
-        rooms.add(msg.topic.split('/')[1]);
+        rooms.add(topic.split('/')[1]);
         if (old_size < rooms.size) {
             drawRooms();
         }
@@ -52,31 +52,13 @@ function appInit() {
     document.getElementById('input_text').addEventListener('keydown', sendText);
 
     // setup the Mqtt client
-    client = new Paho.MQTT.Client(host, 9001, "/", "jsClient"+((Math.random()*100)&100));
-    client.onMessageArrived = (msg) => {
-        try {
-            messageArrived(msg);
-        } catch (e) {
-            console.log("Error", msg, e)
-        }
-    };
-    //client.onConnected = () => console.log('cool!');
-    client.connect({
-        userName: login,
-        password : password,
-        hosts : [host],
-        ports : [9001],
-        reconnect: false,
-        onSuccess: () => {
-            client.subscribe("rooms/#", {
-                onFailure: (...args) => {console.log("Error subscribing:", args)}
-            });
-            showMessages();
-            console.log('init finished.');
-        },
-        onFailure: () => alert('Failed :(')
+    client = new mqtt(`mqtt://${login}:${password}@${host}:9001`);
+    client.on('connect', () => {
+        client.subscribe("rooms/#", (err) => {err && console.log('Error', err)} );
+        showMessages();
+        console.log('init finished.');
     });
-
+    client.on('message', messageArrived);
     drawRooms();
     drawUsers();
     document.getElementById('input_text').focus();
