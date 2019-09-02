5070a35517f909667cedd16e0172cd60404f9db4
fdev31 <fdev31@gmail.com>
2019-09-02 20:47:44 +0200

    add missing -v argument in a comment

--- src/memorize.py
+++ src/memorize.py
@@ -4,7 +4,7 @@
 # <topic> <payload>
 # in standart input or as argument
 # run with:
-#    mosquitto_sub -L 'mqtt://login:pass@hostname:port/rooms/#' | ./memorize.py
+#    mosquitto_sub -v -L 'mqtt://login:pass@hostname:port/rooms/#' | ./memorize.py
 # (replace login, pass & hostname)
 
 import os
