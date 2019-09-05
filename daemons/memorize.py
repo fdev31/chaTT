cccfdc36712eef43d675cde6b97092c72a499f3a
fdev31 <fdev31@gmail.com>
2019-09-05 21:39:32 +0200

    improve the bot feature of memorize
    now comments new arrivals

--- daemons/memorize.py
+++ daemons/memorize.py
@@ -35,6 +35,14 @@ authors = set()
 channels = set()
 messages = dict()
 
+authors_info = dict()
+authors_by_addr = dict()
+
+def set_author_info(author, **kw):
+    if author not in authors_info:
+        authors_info[author] = {}
+    authors_info[author].update(kw)
+
 if os.path.exists(DB_FILE):
     obj = json.load(open(DB_FILE))
     for name in obj['authors']:
@@ -43,6 +51,10 @@ if os.path.exists(DB_FILE):
         channels.add(name)
     for room, log in obj['messages'].items():
         messages[room] = log
+    for author, info in obj.get('authors_info', {}).items():
+        authors_info[author] = info
+    for addr, namelist in obj.get('authors_by_addr', {}).items():
+        authors_by_addr[addr] = set(namelist)
 
 def mqtt_pub(topic, payload):
     subprocess.call(['mosquitto_pub', '-L', 'mqtt://'+login+':'+password+'@localhost:1883/'+topic, '-m', json.dumps(payload)])
@@ -56,9 +68,44 @@ def publish_users():
 def publish_text(room, log):
     requests.post(HTTP_SERVER + 'cmd/setRoomMessages', json={'room': room, 'messages': log})
 
-def publish_ipaddress(user, ip_address):
-    hostname = socket.gethostbyaddr(ip_address)
-    mqtt_pub('rooms/main/newtext', {'author': 'bot', 'text': 'Welcome <b>%s</b>@%s [%s]'%(user, hostname, ip_address)})
+def handle_newcomer(user, data):
+    ip_address = data['ipAddress']
+    hostname = socket.gethostbyaddr(ip_address)[0]
+
+    if ip_address in authors_by_addr:
+        other_names = authors_by_addr[ip_address]
+        other_names.add(user)
+    else:
+        other_names = authors_by_addr[ip_address] = set([user])
+
+    friends = other_names.copy()
+    friends.discard(user)
+
+    message = []
+    ignore = False
+
+    if user not in authors_info:
+        message.append('Welcome <b>%s</b>!!'%user)
+        if friends:
+            message.append('@ %s...'%(', '.join(friends)))
+    else:
+        old_data = authors_info.get(user)
+        if old_data['ip'] == ip_address:
+#             ignore = True # reconnect
+            ignore = False
+        else:
+            message.append('%s, did you change your computer?'%user) # host changed
+        if user not in authors: # was online!
+            message.append('Welcome back online <b>%s</b>! :)'%user)
+        else: # reconnect ?
+            message.append('Having connection issues?')
+
+    if not ignore:
+        message.append('%s@%s[%s]'%(user, hostname, ip_address))
+        mqtt_pub('rooms/main/newtext', {'author': 'bot', 'text': ' '.join(message)})
+
+    print(message, ignore, ip_address, hostname, friends)
+    set_author_info(user, ip=ip_address, host=hostname)
 
 def process_message(topic, message):
     split_topic = topic.split('/')
@@ -82,7 +129,7 @@ def process_message(topic, message):
                 debouncer.schedule(publish_users, LATENCY)
     elif split_topic[0] == 'users':
         if split_topic[2] == 'hello':
-            publish_ipaddress(split_topic[1], obj['ipAddress'])
+            handle_newcomer(split_topic[1], obj)
             old_len = len(authors)
             authors.add(split_topic[1])
             if old_len < len(authors):
@@ -105,5 +152,11 @@ if __name__ == '__main__':
             process_message(topic, message)
     except (Exception, KeyboardInterrupt):
         debouncer.running = False
-        json.dump({'messages': messages, 'authors': list(authors), 'rooms': list(channels)}, open(DB_FILE, 'w'))
+        json.dump({
+            'messages': messages,
+            'authors_by_addr': {k: list(v) for k, v in authors_by_addr.items()},
+            'authors_info': authors_info,
+            'authors': list(authors),
+            'rooms': list(channels)
+            }, open(DB_FILE, 'w'))
 
