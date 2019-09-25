bbf8f69d85f82ec70f850a81151fe2600cf003a1
fdev31 <fdev31@gmail.com>
2019-09-25 22:29:36 +0200

    Be a bit less lazy in the output of genpass

--- scripts/genpass.js
+++ scripts/genpass.js
@@ -2,8 +2,10 @@
 
 const {makeRandomPair} = require('../static/js/randgen');
 
-if (process.argv.length == 3)
-    console.log(makeRandomPair(process.argv[2]));
-else
+if (process.argv.length == 3) {
+    console.log('Login & pass:');
+    console.log(makeRandomPair(process.argv[2]).join('\n'));
+} else {
     console.log(`Syntax: ${process.argv[1].split('/').splice(-1)} "passphrase"`);
+}
 
