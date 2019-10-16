f1bd18036ed517df8141a3744ee9f51a1907ca67
fdev31 <fdev31@gmail.com>
2019-10-16 22:59:06 +0200

    switch to the all in one server

--- mkEnv.sh
+++ mkEnv.sh
@@ -5,4 +5,3 @@ mkdir $D
 python -m venv $D
 source $D/bin/activate
 pip install -r requires.txt
-pip install uwsgi
--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-source pyenv/bin/activate
-exec uwsgi --ini sample_configs/uwsgi.ini
-#exec python ./daemons/server.py
+. pyenv/bin/activate
+exec python ./daemons/server.py
